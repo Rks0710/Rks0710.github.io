2ca587989423c784c49404f35b49eff065568dfd
Rks0710 <63329769+Rks0710@users.noreply.github.com>
2020-05-18 12:54:56 -0400

    Added New Files

---
+++ script.js
@@ -0,0 +1,9 @@
+//For the Swiper	
+var swiper = new Swiper('.swiper-container', {
+      slidesPerView: 1,
+      spaceBetween: 30,
+      pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+      },
+    });
